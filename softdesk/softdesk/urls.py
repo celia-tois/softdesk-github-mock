f933ea055ce0a81fe51537dec97a25eb04127204
Célia Tois <celia.tois@gmail.com>
2022-09-26 12:10:13 +0200

    remove unused imports

--- softdesk/softdesk/urls.py
+++ softdesk/softdesk/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from authentication.views import SignupUserViewset
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
